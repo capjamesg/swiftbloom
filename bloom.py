a70d42d89b803a3bffa6b83bfde90afdbfe4a038
James Gallagher <jamesg@jamesg.blog>
2023-12-24 22:45:57 +0000

    run black

--- bloom.py
+++ bloom.py
@@ -2,11 +2,13 @@ import mmh3
 from typing import Union
 import math
 
+
 def estimate_error_rate(n: int, m: int, k: int):
     # k = hash functions
     # m = # of bits
     # n = # of elements
     return math.pow(1 - math.exp(-k * n / m), k)
+
 
 class SwiftBloom:
     def __init__(self, size: int, k_hash_functions: int = 3):
@@ -29,13 +31,17 @@ class SwiftBloom:
         for i in range(0, self.k_hash_functions):
             hash_function_results.append(mmh3.hash(item, i) % self.size)
 
-        contains_zero_bit = [self.bits[hash_function_result] for hash_function_result in hash_function_results]
+        contains_zero_bit = [
+            self.bits[hash_function_result]
+            for hash_function_result in hash_function_results
+        ]
 
         if False in contains_zero_bit:
             return False
-        
+
         return True
-    
+
+
 names = ["James", "Taylor", "TayTay"]
 
 bloom = SwiftBloom(1000)
